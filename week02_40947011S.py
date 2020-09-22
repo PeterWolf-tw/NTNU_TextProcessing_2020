b14e4547460ce45e3bf3987e85bfffaab79373ab
Beck049 <71216971+Beck049@users.noreply.github.com>
2020-09-22 22:32:55 +0800

    Update week02_40947011S.py

--- week02_40947011S.py
+++ week02_40947011S.py
@@ -5,8 +5,12 @@ def main(inputSTR):
     '''
     這支程式的主要函式(「函式」就是「功能」的意思！)
     '''
-    print("Hello {},".format(inputSTR))
+    print("Hello {}, {}".format(inputSTR, "\n你好呀!"))
+    print("名字:{}".format(inputSTR[0]))
+    print("學號:{}".format(inputSTR[1]))
+ 
 
+    print('\n')
     messageSTR = """
     「程式設計與基礎資料型態與中文構詞學」
     整堂課的資訊量爆炸，在知識的海洋裡衝浪
@@ -17,5 +21,5 @@ def main(inputSTR):
 
 #程式進入點！ week02.py 這支程式從這裡開始「執行」！
 if __name__ == "__main__":
-    nameSTR = "PeterWolf"
+    nameSTR = "蕭文政 40947011S".split(" ")
     main(nameSTR)
