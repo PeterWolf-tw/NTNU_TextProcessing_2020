09b0beb8944c6d4f1f0d9fc098438e5ee3789b97
JOY-8831 <61821412+JOY-8831@users.noreply.github.com>
2020-09-22 17:24:21 +0800

    Add files via upload

---
+++ week02_40623206l.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+
+def main(inputSTR):
+    '''
+    這支程式的主要函式(「函式」就是「功能」的意思！)
+    '''
+    print("Hello {},".format(inputSTR))
+    inputSubSTR =  inputSTR.split(" ")
+    
+    messageSTR = """
+    「程式設計與基礎資料型態與中文構詞學」
+    整堂課的資訊量爆炸，在知識的海洋裡衝浪
+    超過癮的啊啊啊啊！
+    """
+    print(messageSTR)
+    print(inputSubSTR[0])
+    print(inputSubSTR[1])
+
+#程式進入點！ week02.py 這支程式從這裡開始「執行」！
+if __name__ == "__main__":
+    nameSTR = "劉庭妤 40623206l"
+    main(nameSTR)
