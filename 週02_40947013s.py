297121c4f6ec18dcc11eccb3fdfdc1f38a4e73fe
sun-wt <71592704+sun-wt@users.noreply.github.com>
2020-09-24 11:08:12 +0800

    Add files via upload

---
+++ 週02_40947013s.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+#-*-編碼:utf-8-*-
+
+def myfunction (inputSTR):
+    '''
+    這隻程式的主要函式(「函式」就是「功能」的意思！)
+    '''
+    print("Hello{},".format(inputSTR))
+
+    myNameSTR=inputSTR[0:3]
+    myIDSTR=inputSTR[4:]
+
+    print("我的名字:{}".format(myNameSTR))
+    print("我的學號:{}".format(myIDSTR))
+
+
+    inputSTRLIST=inputSTR.split("")
+    print(inputSTRLIST)
+    print("我的名字:{}".format(inputSTRLIST[0]))
+    print("我的學號:{}".format(inputSTRLIST[1]))
+
+
+
+
+
+#程式進入點！ week02.py 這支程式從這裡開始「執行」！
+if __name__ == "__main__":
+    nameSTR = "孫韻婷 40947013S"
+
+    myfunction(nameSTR)
