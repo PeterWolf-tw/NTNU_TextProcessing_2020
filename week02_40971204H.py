0778e28a8e822e6052789f11dc90e315c6ef036e
Min901128 <71401714+Min901128@users.noreply.github.com>
2020-09-29 01:43:31 +0800

    Update week02_40971204H.py

--- week02_40971204H.py
+++ week02_40971204H.py
@@ -1,35 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
-def myfunction(inputSTR):
-    '''
-    這支程式的主要函式(「函式」就是「功能」的意思！)
-    '''
-    print("Hello {}, {}".format(inputSTR, "你好"))
+def min(inputSTR):
 
-    myNameSTR = inputSTR[0:9]
-    myIDSTR = inputSTR[10:20]
+    inputList= inputSTR.split(" ")
+ 
+    print("我的名字：{}".format(inputList[0]))
+    print("我的學號：{}".format(inputList[1]))
 
-    print("我的名字:{}".format(myNameSTR))
-    print("我的學號:{}".format(myIDSTR))
-
-
-    inputSTRLIST = inputSTR.split(" ")
-    print(inputSTRLIST)
-    print("我的名字：{}".format(inputSTRLIST[0]))
-    print("我的學號：{}".format(inputSTRLIST[1]))
-
-
-    messageSTR = """
-    「程式設計與基礎資料型態與中文構詞學」
-    整堂課的資訊量爆炸，在知識的海洋裡衝浪
-    超過癮的啊啊啊啊！
-    """
-    #print(messageSTR)
 
 
 #程式進入點！ week02.py 這支程式從這裡開始「執行」！
 if __name__ == "__main__":
-    nameSTR = "PeterWolf F123454321"
+    nameSTR = "黃敏智 40971204H"
 
-    myfunction(nameSTR)
+    min(nameSTR)
