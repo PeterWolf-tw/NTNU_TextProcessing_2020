02867d5951969782bfb81bd91a9585c8918c02e3
tzminch <71556663+tzminch@users.noreply.github.com>
2020-10-06 22:17:24 +0800

    Update week04_40621132L.py

--- week04/week04_40621132L.py
+++ week04/week04_40621132L.py
@@ -6,16 +6,15 @@ def openfile(txtFILE):
         txtSTR = f.read()
     return txtSTR
 
-
 if __name__ == '__main__':
-    fileTUPLE = ("example/dbp.txt", "example/pbd.txt")
+    txtfileTUPLE = ("example/dbp.txt","example/pbd.txt")
 
-    for i in range (2):
-        resultSTR = openfile(fileTUPLE[i])
+    for i in range (len(txtfileTUPLE)):
+        resultSTR = openfile(txtfileTUPLE[i])
         
         xINT = resultSTR.count("婦人")
         yINT = resultSTR.count("土狗")
         zINT = resultSTR.count("男")
         countLIST = [("婦人", xINT), ("土狗", yINT), ("男", zINT)]
-        print(fileTUPLE[i].split("/")[1], countLIST)
+        print(txtfileTUPLE[i].split("/")[1], ":", countLIST)
 
