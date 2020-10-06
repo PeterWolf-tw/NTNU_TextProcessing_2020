36563c24623c8811b34c171654d2fc5c90396b68
tzminch <71556663+tzminch@users.noreply.github.com>
2020-10-06 22:22:36 +0800

    Update week04_40621132L.py

--- week04/week04_40621132L.py
+++ week04/week04_40621132L.py
@@ -6,15 +6,18 @@ def openfile(txtFILE):
         txtSTR = f.read()
     return txtSTR
 
-if __name__ == '__main__':
-    txtfileTUPLE = ("example/dbp.txt","example/pbd.txt")
-
-    for i in range (len(txtfileTUPLE)):
-        resultSTR = openfile(txtfileTUPLE[i])
+def wordcount(TUPLE):
+    for i in range (len(TUPLE)):
+        resultSTR = openfile(TUPLE[i])
         
         xINT = resultSTR.count("婦人")
         yINT = resultSTR.count("土狗")
         zINT = resultSTR.count("男")
         countLIST = [("婦人", xINT), ("土狗", yINT), ("男", zINT)]
-        print(txtfileTUPLE[i].split("/")[1], ":", countLIST)
+        print(TUPLE[i].split("/")[1], ":", countLIST)
+    
+
+if __name__ == '__main__':
+    txtfileTUPLE = ("example/dbp.txt","example/pbd.txt")
+    wordcount(txtfileTUPLE)
 
