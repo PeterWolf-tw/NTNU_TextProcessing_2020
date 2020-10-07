bcd399d01004f9856269bc52f63e984641e7190d
anci <anci.peng@gmail.com>
2020-10-07 13:44:11 +0800

    Create week04_40947019s.py

---
+++ week04/week04_40947019s.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8 _*_
+def main(textFILE):
+    with open(textFILE,encoding="UTF-8") as f:
+        txt = f.read()
+    return txt
+if __name__=='__main__':
+    fileTUPLE=("example/dbp.txt","example/pbd.txt")
+    for i in range (2):
+        resultSTR = main(fileTUPLE[i])
+        xINT = resultSTR.count("婦人")
+        yINT = resultSTR.count("土狗")
+        zINT = resultSTR.count("男")
+        dbpLIST = [("婦人", xINT), ("土狗", yINT), ("男", zINT)]
+        
+        print(fileTUPLE[i].split("/")[1], dbpLIST)
