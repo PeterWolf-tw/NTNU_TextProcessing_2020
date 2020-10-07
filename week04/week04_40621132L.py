5da8d1a6bec61b3ea008cd31b22283bcad01cd6a
tzminch <71556663+tzminch@users.noreply.github.com>
2020-10-07 23:33:03 +0800

    Update week04_40621132L.py

--- week04/week04_40621132L.py
+++ week04/week04_40621132L.py
@@ -6,18 +6,26 @@ def openfile(txtFILE):
         txtSTR = f.read()
     return txtSTR
 
-def wordcount(TUPLE):
-    for i in range (len(TUPLE)):
-        resultSTR = openfile(TUPLE[i])
+#def wordcount(TUPLE):
+    #for i in range (len(TUPLE)):
+        #resultSTR = openfile(TUPLE[i])
+        
+        #xINT = resultSTR.count("婦人")
+        #yINT = resultSTR.count("土狗")
+        #zINT = resultSTR.count("男")
+        #countLIST = [("婦人", xINT), ("土狗", yINT), ("男", zINT)]
+        #print(TUPLE[i].split("/")[1], ":", countLIST)
+    
+
+if __name__ == '__main__':
+    txtfileTUPLE = ("example/dbp.txt","example/pbd.txt")
+    #wordcount(txtfileTUPLE)
+    
+    for i in range (len(txtfileTUPLE)):
+        resultSTR = openfile(txtfileTUPLE[i])
         
         xINT = resultSTR.count("婦人")
         yINT = resultSTR.count("土狗")
         zINT = resultSTR.count("男")
         countLIST = [("婦人", xINT), ("土狗", yINT), ("男", zINT)]
-        print(TUPLE[i].split("/")[1], ":", countLIST)
-    
-
-if __name__ == '__main__':
-    txtfileTUPLE = ("example/dbp.txt","example/pbd.txt")
-    wordcount(txtfileTUPLE)
-
+        print(txtfileTUPLE[i].split("/")[1], ":", countLIST)
