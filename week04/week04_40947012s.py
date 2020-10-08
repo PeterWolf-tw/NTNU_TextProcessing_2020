36c26505b17c626d1b5be482f0eed16af5294d99
Chih-Yu0917 <selina221947@gmail.com>
2020-10-08 10:26:13 +0800

    Create week04_40947012s.py

---
+++ week04/week04_40947012s.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+def main(text):
+        with open(text, encoding="utf-8") as f:
+               txt = f.read()
+        return txt
+
+
+
+if __name__ == "__main__":
+    a = ("./example/dbp.txt", "./example/pbd.txt")
+    
+    result = main(a[0])
+    result = main(a[1])
+    x = result.count("婦人")
+    y = result.count("土狗")
+    z = result.count("男")
+    dbp = [("婦人", x), ("土狗", y), ("男", z)]
+    pbd = [("婦人", x), ("土狗", y), ("男", z)]
+    print("ddp:",dbp,", pbd:", pbd)
+   # print(", pbd:", pbd)
+
+
+    #result = main(a[1])
+    #x = result.count("婦人")
+   # y = result.count("土狗")
+   # z = result.count("男")
+   # pbd = [("婦人", x), ("土狗", y), ("男", z)]
+   # print(", pbd:", pbd)
+
+    #,a[0].split("/")[1][7:7]
+    #,a[1].split("/")[1][7:7]
