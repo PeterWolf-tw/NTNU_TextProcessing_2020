37c91549590cc0da21d794e1e04f6dd7f9bf0742
Ethe1Chen <71498132+Ethe1Chen@users.noreply.github.com>
2020-10-10 17:26:37 +0800

    week04_40784207i.py

---
+++ week04/week04_40784207I.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+
+def main(txtFILE):
+    with open(txtFILE, encoding="utf-8") as F:
+        txtSTR = F.read()
+    return txtSTR
+
+
+if __name__== "__main__":
+    fileTUPLE = ("example/dbp.txt", "example/pbd.txt")
+    
+    resultSTR = main(fileTUPLE[0])
+
+    xINT = resultSTR.count("婦人")
+    yINT = resultSTR.count("土狗")
+    zINT = resultSTR.count("男")
+    dbpLIST = [("婦人", xINT), ("土狗", yINT), ("男", zINT)]
+    print(fileTUPLE[0].split("/")[1], dbpLIST)
+
+
+    resultSTR = main(fileTUPLE[1])
+    
+    xINT = resultSTR.count("婦人")
+    yINT = resultSTR.count("土狗")
+    zINT = resultSTR.count("男")
+    dbpLIST = [("婦人", xINT), ("土狗", yINT), ("男", zINT)]
+    print(fileTUPLE[1].split("/")[1], dbpLIST)
