e95ce10797ddd2ed204a7d132dbe23bfce9f1b98
machipriest <3253qqz@gmail.com>
2020-10-17 22:58:43 +0800

    Create week05_40971216H.py

---
+++ week05/week05_40971216H.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+def cCommandCoRefResolver(inputLIST, coRefKeySTR, personSTR):
+    #LIST輸入
+    "給定要做消解的字串，利用 c-command 定理濾除不可能的人名，回傳可能的指代字串"
+    "[注意]：這只是極度簡化，做為初步教學說明的版本！"
+
+    if coRefKeySTR in inputLIST:
+        pass
+    else:
+        raise ValueError
+
+    if personSTR in inputLIST:
+        pass
+    else:
+        raise ValueError
+
+    personSTRIndex = inputLIST.index(personSTR)
+    if inputLIST[personSTRIndex+len(personSTR)] == "的":
+        return None
+    elif inputLIST[personSTRIndex+len(personSTR)] == "之":
+        return None
+    else:
+        pass
+
+    coRefKeyIndex = inputLIST.index(coRefKeySTR)
+    if coRefKeyIndex > personSTRIndex:
+        return True
+    else:
+        return False
+
+def coRefResolver(inputSTR, coRefKeySTR, personSTR):
+    "給定要做消解的字串，回傳是否可能為指代字串"
+    if coRefKeySTR in inputSTR:
+        pass
+    else:
+        raise ValueError
+
+    if personSTR in inputSTR:
+        pass
+    else:
+        raise ValueError
+
+    personSTRIndex = inputSTR.index(personSTR)
+    coRefKeyIndex = inputSTR.index(coRefKeySTR)
+    if coRefKeyIndex > personSTRIndex:
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+
+    #inputSTR = "小夫告訴大雄其實靜香喜歡的是他 "
+    #inputSTR = "大雄知道靜香喜歡的是自己 "
+    #inputSTR = "大雄聽胖虎說靜香愛的是自己 "
+    #inputSTR = "大雄聽胖虎的妹妹說靜香愛的是自己"
+    inputLIST = ["大雄", "聽", "胖虎", "的", "妹妹", "說", "靜香", "愛", "的", "是", "自己"]
+    coRefDICT = {inputLIST[10]:[]}
+
+    resultBOOL = cCommandCoRefResolver(inputLIST, inputLIST[10], inputLIST[0])
+    if resultBOOL == True:
+        coRefDICT[inputLIST[10]].append(inputLIST[0])
+    elif resultBOOL == None:
+        pass
+    else:
+        pass
+
+    resultBOOL = cCommandCoRefResolver(inputLIST, inputLIST[10], inputLIST[6])
+    if resultBOOL == True:
+        coRefDICT[inputLIST[10]].append(inputLIST[6])
+    elif resultBOOL == None:
+        pass
+    else:
+        pass
+
+    resultBOOL = cCommandCoRefResolver(inputLIST, inputLIST[10], inputLIST[2])
+    if resultBOOL == True:
+        coRefDICT[inputLIST[10]].append(inputLIST[2])
+    elif resultBOOL == None:
+        pass
+    else:
+        pass
+
+    resultBOOL = cCommandCoRefResolver(inputLIST, inputLIST[10], inputLIST[4])
+    if resultBOOL == True:
+        coRefDICT[inputLIST[10]].append(inputLIST[4])
+    elif resultBOOL == None:
+        pass
+    else:
+        pass
+
+    print(coRefDICT)
