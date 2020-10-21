1a02105599d502ab7787e82243e9a56ca9e91471
Birkhoff Lee <git@birkhoff.me>
2020-10-21 12:03:22 +0800

    Remove debugging code

--- week06/week06_40947026s.py
+++ week06/week06_40947026s.py
@@ -3,6 +3,7 @@
 
 import json, re
 
+
 #讀取 json 的程式
 def jsonTextReader(jsonFilePath):
   with open(jsonFilePath, encoding='utf-8-sig') as f:
@@ -17,23 +18,19 @@ def text2Sentence(inputSTR):
   return text.split('###')
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
   #設定要讀取的 news.json 路徑
   newsJsonPath = "./example/news.json"
 
   #將 news.json 利用 [讀取 json] 的程式打開
   #將讀出來的內容字串傳給 [將字串轉為「句子」 列表」]的程式，存為 newsLIST
   newsLIST = text2Sentence(jsonTextReader(newsJsonPath)["text"])
-  
-  print(newsLIST)
 
   #設定要讀取的 test.json 路徑
   testJsonPath = "./example/test.json"
 
   #將 test.json 的 sentenceLIST 內容讀出，存為 testLIST
   testLIST = jsonTextReader(testJsonPath)["sentence"]
-
-  print(testLIST)
 
   #測試是否達到作業需求
   if newsLIST == testLIST:
