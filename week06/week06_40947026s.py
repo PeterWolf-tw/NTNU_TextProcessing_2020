6e74cb8b0bba526548bc265277eadf47083ef246
Birkhoff Lee <git@birkhoff.me>
2020-10-21 11:45:04 +0800

    Fix text2Sentence
    Signed-off-by: Birkhoff Lee <git@birkhoff.me>

--- week06/week06_40947026s.py
+++ week06/week06_40947026s.py
@@ -11,9 +11,9 @@ def jsonTextReader(jsonFilePath):
 
 #將字串轉為「句子」列表的程式
 def text2Sentence(inputSTR):
-  separatorRegexp = r'(。|，|、|(?<![0-9]),(?![0-9])|(?<![0-9]),(?!\D))'
-  text = re.sub(r'。$|…', '', inputSTR)
-  text = re.sub(separatorRegexp, '###', text).replace("...", "")
+  separatorRegexp = r'(。(?!$)|，|、|(?<![0-9]),(?![0-9])|(?<![0-9]),(?!\D))'
+  text = re.sub(r'(…|\...)', '', inputSTR)
+  text = re.sub(separatorRegexp, '###', text)
   return text.split('###')
 
 
