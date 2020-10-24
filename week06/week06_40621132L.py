7feba391e4bea0a7ba9f9964162ca2e617fb1c3a
Chih-Yu0917 <selina221947@gmail.com>
2020-10-24 19:47:16 +0800

    Update week06_40621132L.py

--- week06/week06_40621132L.py
+++ week06/week06_40621132L.py
@@ -11,25 +11,33 @@ def jsonTextReader(jsonFilePath):
 
 #將字串轉為「句子」列表的程式
 def text2Sentence(inputSTR):
-    for item in ("，", "、", "."):
+    for item in ("，", "、", ".", "。"):
         inputSTR = inputSTR.replace(item, "{}".format("#"))
-        #inputSTR = inputSTR.replace(item, >"#")
+        #inputSTR = inputSTR.replace(item, "#")
 
     for i in range(len(inputSTR)):
         if inputSTR[i-1].isdigit() == True and inputSTR[i] == "," and inputSTR[i+1].isdigit() == True:
             pass
         elif inputSTR[i] == ",":
             inputSTR = inputSTR[:i] + "#" +inputSTR[i+1:]
-            
-        if inputSTR[i] == "。" and i != len(inputSTR)-1:
-            inputSTR = inputSTR[:i] +"#" +inputSTR[i+1:]
-    
+        
+        #移除切割最後一個句點剩下的空格方法1
+        #if inputSTR[i] == "。" and i != len(inputSTR)-1:
+            #inputSTR = inputSTR[:i] +"#" +inputSTR[i+1:]
+        #elif inputSTR[i] == "。" and i == len(inputSTR)-1:
+            #inputSTR = inputSTR[:i]         
+        
     while "###" in inputSTR:
         inputSTR = inputSTR.replace("###", "")
     while "…" in inputSTR:
         inputSTR = inputSTR.replace("…", "")
         
-    inputLIST = inputSTR.split("#")    
+    inputLIST = inputSTR.split("#")
+    
+    #移除切割最後一個句點剩下的空格方法2
+    while "" in inputLIST:
+        inputLIST.remove("")
+    
         
     return inputLIST
         
@@ -42,14 +50,14 @@ if __name__== "__main__":
 
     #將讀出來的內容字串傳給 [將字串轉為「句子」 列表」]的程式，存為 newsLIST
     newsLIST = text2Sentence(newsSTR)
-    #print(newsLIST)
+    print("newsLIST: ", newsLIST)
 
     #設定要讀取的 test.json 路徑
     jsonFilePath2 = "./example/test.json"
 
     #將 test.json 的 sentenceLIST 內容讀出，存為 testLIST
     testLIST = jsonTextReader(jsonFilePath2)["sentence"]
-    #print(testLIST)
+    print("testLIST: ", testLIST)
 
     #測試是否達到作業需求
     if newsLIST == testLIST:
