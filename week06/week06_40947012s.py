88dda92b9b74bace365e99ec32fbae7a73a6921c
Chih-Yu0917 <selina221947@gmail.com>
2020-10-24 20:18:58 +0800

    Revert "Revert "finish my hw""
    This reverts commit dfdec2353001f8583445bde0aecbe989f4b04e0f.

--- week06/week06_40947012s.py
+++ week06/week06_40947012s.py
@@ -15,34 +15,34 @@ def jsonTextReader(jsonFilePath):
 def text2Sentence(inputSTR):
     for item in("...","…"):
         inputSTR=inputSTR.replace(item,'')
-    for item in("、"):
+    for item in("、","，","。"):
         inputSTR=inputSTR.replace(item,"<My Cutting Mark>")
     for i in range(len(inputSTR)):
-        if inputSTR[i]==',' and inputSTR[i-1] =="5":
-            inputSTR = inputSTR[:i-1] + "<My Cutting Mark>" + inputSTR[i:]
-        if inputSTR[i]==',' and inputSTR[i-1] not in ['0','1','2','3','4','6','7','8','9']:
+#        if inputSTR[i]==',' and inputSTR[i-1] =="5":
+#            inputSTR = inputSTR[:i-1] + "<My Cutting Mark>" + inputSTR[i:]
+        if inputSTR[i]==',' and inputSTR[i-1] not in ['0','1','2','3','4','5','6','7','8','9']:
             inputSTR = inputSTR[:i] + "<My Cutting Mark>" + inputSTR[i+1:]
-        if inputSTR[i] == "。" and i != len(inputSTR)-1:
-            inputSTR = inputSTR[:i] +"<My Cutting Mark>" + inputSTR[i+1:]
-    inputLIST = inputSTR.split("<My Cutting Mark>")
+#        if inputSTR[i] == "。" and i != len(inputSTR)-1:
+#            inputSTR = inputSTR[:i] +"<My Cutting Mark>" + inputSTR[i+1:]
+    inputLIST = inputSTR.split("<My Cutting Mark>")[:-1]
     return inputLIST
 
 if __name__== "__main__":
     #設定要讀取的 news.json 路徑
-    jsonFilePath="./example/news.json"
+    jsonFilePath1="./example/news.json"
 
     #將 news.json 利用 [讀取 json] 的程式打開
-    newsSTR=jsonTextReader(jsonFilePath)["text"]
+    newsSTR=jsonTextReader(jsonFilePath1)["text"]
 
     #將讀出來的內容字串傳給 [將字串轉為「句子」 列表」]的程式，存為 newsLIST
     newsLIST=text2Sentence(newsSTR)
-    print(newsLIST)
+    
     #設定要讀取的 test.json 路徑
-    jsonFilePath="./example/test.json"
+    jsonFilePath2="./example/test.json"
 
     #將 test.json 的 sentenceLIST 內容讀出，存為 testLIST
-    testLIST=jsonTextReader(jsonFilePath)["sentence"]
-    print(testLIST)
+    testLIST=jsonTextReader(jsonFilePath2)["sentence"]
+    
     #測試是否達到作業需求
     if newsLIST == testLIST:
         print("作業過關！")
