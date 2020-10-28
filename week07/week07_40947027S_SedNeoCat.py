94503848294fa42910d91d52b119a41135a6be48
Adam7066 <adam20001002@gmail.com>
2020-10-28 21:55:43 +0800

    Update Week07_40947027S_SedNeoCat

--- week07/week07_40947027S_SedNeoCat.py
+++ week07/week07_40947027S_SedNeoCat.py
@@ -5,7 +5,19 @@ import os, json, jieba
 def text2cws(jsonFilePath):
     with open(jsonFilePath, 'r', encoding='utf-8') as f:
         js = json.loads(f.read())
-    return list(jieba.cut(js["BODY"]))
+    jsBodyStr = js["BODY"]
+    for item in ("、", "，", "。", "「", "」", "/", "／", "(", ")", "（", "）"):
+        jsBodyStr = jsBodyStr.replace(item, '<cutting mark>')
+    for item in ("...", "…"):
+        jsBodyStr = jsBodyStr.replace(item, '')
+    for i in range(len(jsBodyStr)):
+        if jsBodyStr[i] == "," and jsBodyStr[i-1]not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+            jsBodyStr = jsBodyStr[:i]+"<cutting mark>"+jsBodyStr[i+1:]
+    jsBodyList = jsBodyStr.split('<cutting mark>')[:-1]
+    rList= []
+    for i in jsBodyList:
+        rList.extend(list(jieba.cut(i)))
+    return rList
 
 def termFreq(jiebaList):
     tempDict = {}
@@ -20,8 +32,10 @@ if __name__ == "__main__":
     path = ('./example/health', './example/finance')
     resultDict = [{}, {}]
     for i in range(len(path)) :
+        forTermList = []
         dirs = os.listdir(path[i])
         for j in dirs:
-            resultDict[i] = termFreq(text2cws(path[i] + '/' + j))
+            forTermList.extend(text2cws(path[i] + '/' + j))
+        resultDict[i] = termFreq(forTermList)
     for i in resultDict:
         print(i)
