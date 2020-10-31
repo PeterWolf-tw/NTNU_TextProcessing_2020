8a8c93af67cfb119e50accc1a1b3e8554e4b8496
OvOb-owo <72025241+OvOb-owo@users.noreply.github.com>
2020-11-01 01:45:18 +0800

    week07 hw fixed

--- week07/week07_40976013H_squad.py
+++ week07/week07_40976013H_squad.py
@@ -1,60 +1,63 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 
+import json, os, jieba
 
 #讀取 json 檔案，並以 jieba 斷詞處理其內容中 "BODY" 欄位的程式
-#[註] 上週才寫過「讀取 json 的檔案，其實可以把它複製過來使用哦！
-import json,os,jieba
-#這麼一來， "讀取 json" 的功能就不用重寫了。只要把檔案丟給上週的
-#程式，取得回傳的值，再接著寫就好了。
-def text2cws(foldername):
-    def jsontextreader(jsonFilePath):
-        with open (jsonFilePath,"r",encoding="utf-8") as f:
-            jsonContent=json.load(f)
+#[註] 上週才寫過讀取 json 的檔案，其實可以把它複製過來使用哦！
+
+def text2cws(folderName):
+    def jsonTextReader(jsonFilePath):
+        with open (jsonFilePath, "r", encoding = "utf8") as f:
+            jsonContent = json.load(f)
             return jsonContent
+    
+    resultLIST = []
+    sentenceLIST = []
+    joinLIST = []
+    for file in os.listdir(folderName):
+        jsonFilePath = folderName + "/" + file
         
-    resultLIST=[]
-    sentenceLIST=[]
-    for file in os.listdir(foldername):
-        jsonFilePath=foldername+"/"+file
+        newsSTR = jsonTextReader(jsonFilePath)["BODY"]
+        #print (newsSTR)
         
-        newsSTR=jsontextreader(jsonFilePath)["BODY"]
         for item in ("，", "、", "。", "「", "」", "【", "】", "（", "）", "；", "-"):
             newsSTR = newsSTR.replace(item, "{}".format("#"))
-            
         for i in range(len(newsSTR)):
-                   #千位數
+            #千位數
             if newsSTR[i-1].isdigit() == True and newsSTR[i] == "," and newsSTR[i+1].isdigit() == True:
-                 pass
+                pass
             elif newsSTR[i] == ",":
                 newsSTR = newsSTR[:i] + "#" + newsSTR[i+1:]
-                   #小數點
+            #小數點
             if newsSTR[i-1].isdigit() == True and newsSTR[i] == "." and newsSTR[i+1].isdigit() == True:
-                 pass
+                pass
             elif newsSTR[i] == ".":
                 newsSTR = newsSTR[:i] + "#" + newsSTR[i+1:]
-                       
+                
         while "###" in newsSTR:
             newsSTR = newsSTR.replace("###", "")
         while "…" in newsSTR:
             newsSTR = newsSTR.replace("…", "")           
-                           
+                    
         newsLIST = newsSTR.split("#")
         while "" in newsLIST:
             newsLIST.remove("")
-                 
+            
         for s in newsLIST:
             sentenceLIST.append(s)
-        #print(sentenceLIST)
-           
+    #print(sentenceLIST)
+    
     for s in sentenceLIST:
         resultLIST.append("/".join(jieba.cut(s)))  
-           #print(resultLIST)
-           
-    joinLIST = "".join(resultLIST).split("/")
+    print(resultLIST)
+    
+    for i in resultLIST:
+        splitLIST = i.split("/")
+        joinLIST.extend(splitLIST)
+    
     print(joinLIST)
     return joinLIST
-    
 #設計一個名為 termFreq() 的程式，承接 text2cws() 的回傳值，並
 #建立一個 resultDICT{}。內容是 resultDICT = {"某個字/詞", 5,
 #"另一個字/詞", 8} 的格式。其中的數字是那個字/詞在 10 篇文章中總
