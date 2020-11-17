53bf86755f3906977cc463386574ba1770575c1a
AoK_Blast <i19780219111@kimo.com>
2020-11-17 23:03:06 +0800

    DEADLINE

--- week09/deadline/week09_deadline.py
+++ week09/deadline/week09_deadline.py
@@ -9,24 +9,58 @@ def jsonTextReader(jsonFilePath):
     with open(jsonFilePath,"r",encoding="utf-8") as f:
         resultSTR = json.load(f)
     return resultSTR
+def jsonFileWriter(jsonList,jsonFileName):
+    with open(jsonFileName,"w",encoding="utf-8") as f:
+        json.dump(jsonList, f , ensure_ascii=False)
+    return None
+
+def getLIST(inputLIST):
+    getLIST=[]
+    for i in inputLIST:
+        if i != [] :
+            for k in i:
+                if i[0][2] not in getLIST:
+                    getLIST.append(k[2])
+    return getLIST
+
+def countName(inputLISt):
+    countDICT={}
+    for i in inputLISt:
+        if i != []:
+            for k in i:
+                if k[2] in countDICT:
+                    countDICT[k[2]]+=1
+                else:
+                    countDICT[k[2]]=1
+    return list(countDICT.items())
 
 if __name__== "__main__":
     articut=ArticutAPI.Articut()
    
-    jsonFilePath = '../example/tourblog.json'
-    newsSTR = jsonTextReader(jsonFilePath)["content"]
+    tourFilePath = '../example/tourblog.json'
+    newsSTR = jsonTextReader(tourFilePath)["content"]
+    newsLIST = articut.parse(newsSTR, level = "lv2")
+    locLIST = getLIST(articut.getLocationStemLIST(newsLIST))
+    newsLIST = articut.parse(newsSTR,openDataPlaceAccessBOOL=True)
+    placeLIST = getLIST(articut.getOpenDataPlaceLIST(newsLIST))
+    tourblog="tourblog_geoinfo.json"
+    jsonFileWriter({"location": locLIST, "place": placeLIST},tourblog)  
+
+    criminalFilePath = '../example/刑事判決_106,交簡,359_2017-02-21.json'
+    newsSTR = jsonTextReader(criminalFilePath)["mainText"]
     newsLIST = articut.parse(newsSTR,level="lv2")
-    locLIST = articut.getLocationStemLIST(newsLIST)
-    placeLIST = articut.parse(locLIST,openDataPlaceAccessBOOL=True)
-    resultDICT = articut.getOpenDataPlaceLIST(placeLIST)
-    
-    jsonFilePath = '../example/刑事判決_106,交簡,359_2017-02-21.json'
-    newsSTR = jsonTextReader(jsonFilePath)["mainText"]
-    newsLIST = articut.parse(newsSTR,level="lv2")
-    lawLIST = ArticutAPI.LawsToolkit(resultDICT)
-    
+    lawLIST = ArticutAPI.LawsToolkit(newsLIST).getCrime()
+    justice="jusitice.json"
+    jsonFileWriter({"liability": lawLIST},justice)    
+
     jsonFilePath='../example/news.json'
     newsSTR = jsonTextReader(jsonFilePath)["content"] 
     newsLIST = articut.parse(newsSTR,level="lv2")
     nameLIST = articut.getPersonLIST(newsLIST)
-    locLIST = articut.getLocationStemLIST(nameLIST)
+    nameCountLIST=countName(nameLIST)
+    locLIST = getLIST(articut.getLocationStemLIST(newsLIST))
+    moneyLIST=getLIST(articut.getCurrencyLIST(newsLIST))
+    newsInfo="news_info.json"
+    newsInfoContent={"people":nameCountLIST,"location":locLIST, "money":moneyLIST}
+    jsonFileWriter(newsInfoContent,newsInfo)
+    
