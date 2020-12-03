1a323aaa8aa7b61341e7ac923cf98cd7ab105de1
Adam7066 <adam20001002@gmail.com>
2020-12-03 23:23:21 +0800

    update week12_SedNeoCat

--- week12/SedNeoCat/week12_SedNeoCat.py
+++ week12/SedNeoCat/week12_SedNeoCat.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
-import sys
-import typing
+import sys, typing, json
 sys.path.append("../../week09/SedNeoCat/")
 
-
 def txtExtractor(path: str):
-    with open(path) as f:
+    with open(path, encoding="utf-8") as f:
         return f.readlines()
-
 
 def text2Sentence(inputSTR: str):
     for item in ("、", "，", "。"):
@@ -15,11 +12,10 @@ def text2Sentence(inputSTR: str):
     for item in ("...", "…"):
         inputSTR = inputSTR.replace(item, '')
     for i in range(len(inputSTR)):
-        if inputSTR[i] == "," and inputSTR[i-1]not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+        if inputSTR[i] == "," and inputSTR[i-1] not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
             inputSTR = inputSTR[:i]+"<cutting mark>"+inputSTR[i+1:]
     inputLIST = inputSTR.split('<cutting mark>')[:-1]
     return inputLIST
-
 
 def keywordFilter(target: str, input: typing.List[str]):
     ret = []
@@ -29,6 +25,9 @@ def keywordFilter(target: str, input: typing.List[str]):
             ret.append(x[pos:])
     return ret
 
+def createJson(jsonPath, inputDict):
+    with open(jsonPath, 'w', encoding='utf-8') as f:
+        json.dump(inputDict, f, indent=4, ensure_ascii=False)
 
 if __name__ == "__main__":
     from ArticutAPI import ArticutAPI
@@ -39,7 +38,7 @@ if __name__ == "__main__":
         tmp = articut.parse(txt, level="lv3")
         for x in tmp["event"]:
             if len(x) > 0:
-                result["倉鼠"].append(tuple(reversed(x)))
+                result["倉鼠"].append(tuple(x))
     penguinRawTxt = txtExtractor("penguin.txt")
     penguinSetence = []
     for x in penguinRawTxt:
@@ -49,5 +48,6 @@ if __name__ == "__main__":
         tmp = articut.parse(txt, level="lv3")
         for x in tmp["event"]:
             if len(x) > 0:
-                result["皇帝企鵝"].append(tuple(reversed(x)))
+                result["皇帝企鵝"].append(tuple(x))
     print(result)
+    createJson("week12_SedNeoCat.json", result)
