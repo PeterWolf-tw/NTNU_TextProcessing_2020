67d058f01ca4e664262f1c29262b5d8371bc833d
jieshu011 <71324132+jieshu011@users.noreply.github.com>
2020-12-09 01:51:54 +0800

    week12_平均大4+.py // week12_平均大4+.json
    修改week12_平均大4+.py
    上傳week12_平均大4+.json

--- week12/平均大四+/week12_平均大4+.py
+++ week12/平均大四+/week12_平均大4+.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+import json
 def txtReader(filepath):
     with open(filepath, encoding="utf-8") as f:
         return f.read()
@@ -26,30 +27,36 @@ if __name__ == "__main__":
     from ArticutAPI import ArticutAPI
     myArti = ArticutAPI.Articut()
     FilePaths = ["example/text.txt", "example/pentext.txt"]
-    jsonFileName = ["week12_平均大4+"]
+    jsonFileName = "week12_平均大4+.json"
     jsonFileObject = ["倉鼠", "皇帝企鵝"]
 
     resultEventLIST = []
     for p in FilePaths:
-    ##取 event 和 verb list##  #已經沒有字數可分析了...所以無法建檔
-        print(myArti.parse(txtReader(p), level="lv3"))#確認用
-        Event = getItemLIST(myArti.parse(txtReader(p), level="lv3")["event"])
-        Dict = myArti.parse(txtReader(p), level="lv2")
-        VList = getItemLIST(myArti.getVerbStemLIST(Dict))
+    ##取 event 和 verb list##
+        try:
+            print(myArti.parse(txtReader(p), level="lv3"))#確認用
+            Event = getItemLIST(myArti.parse(txtReader(p), level="lv3")["event"])
+            Dict = myArti.parse(txtReader(p), level="lv2")
+            VList = getItemLIST(myArti.getVerbStemLIST(Dict))
+        except MaxRetryError and ConnectionError:
+            pass
         resultEventLIST.append(Event)
         print("Event:", Event)#確認用
         print("VList:", VList)#確認用
     ##比較不同之處並印出##
         ranELIST = []
         for i in Event:
-            ranELIST.append(i[0])
-            ranELIST.append(i[1])
+            if len(i)==2:
+                ranELIST.append(i[0])
+                ranELIST.append(i[1])
+            else:
+                ranELIST.append(i)
         DiffLIST = []
         for i in VList:
             if i not in ranELIST:
                 DiffLIST.append(i)
         print("event未提及的動詞：", DiffLIST)
     ##建檔##
-    jsonWriter(jsonFileName[0], jsonFileObject, resultEventLIST)
+    jsonWriter(jsonFileName, jsonFileObject, resultEventLIST)
 
     print(resultEventLIST)#確認用
